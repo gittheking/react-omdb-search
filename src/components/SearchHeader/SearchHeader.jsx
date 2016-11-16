c6702ad5fb94d2e7ca301842db9aab319b7cb4ec
Bobby King <king.robertjoseph@gmail.com>
2016-11-16 10:46:25 -0500

    Update SearchHeader.jsx to be a stateless component

--- src/components/SearchHeader/SearchHeader.jsx
+++ src/components/SearchHeader/SearchHeader.jsx
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../../logo.svg';
 import './SearchHeader.css';
 
-class SearchHeader extends Component {
-  handleKeyPress(e) {
-    if (e.key === 'Enter') this.props.handleSubmitSearch();
-  }
-
-  render() {
-    return (
-      <div>
-        <header>
-          <img src={logo} alt="React Logo" />
-          <h1>OMDB Search</h1>
-        </header>
-        <div id="search-container">
-          <input
-            type="text"
-            value={this.props.searchTerm}
-            onChange={this.props.handleUpdateSearch}
-            onKeyPress={event => this.handleKeyPress(event)}
-            placeholder="Search by movie title..."
-          />
-          <button onClick={this.props.handleSubmitSearch}>Search</button>
-        </div>
-      </div>
-    );
-  }
+function handleKeyPress(e, cb) {
+  if (e.key === 'Enter') cb();
 }
+
+const SearchHeader = props => (
+  <div>
+    <header>
+      <img src={logo} alt="React Logo" />
+      <h1>OMDB Search</h1>
+    </header>
+    <div id="search-container">
+      <input
+        type="text"
+        value={props.searchTerm}
+        onChange={props.handleUpdateSearch}
+        onKeyPress={event => handleKeyPress(event, props.handleSubmitSearch)}
+        placeholder="Search by movie title..."
+      />
+      <button onClick={props.handleSubmitSearch}>Search</button>
+    </div>
+  </div>
+);
 
 export default SearchHeader;
